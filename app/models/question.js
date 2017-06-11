272b36b7f079bc3373cd72719342347ead8bb6ca
lewis nyoike <nyoikelewis@gmail.com>
2017-06-12 00:13:10 +0300

    updated the model to contain or have many answers

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 author:DS.attr(),
 date:DS.attr(),
-question:DS.attr()
+question:DS.attr(),
+answers:DS.hasMany('question', { async: true})
 });

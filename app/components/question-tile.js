33640178c5786a99221205cb5f1665b2700d346c
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 19:12:16 +0300

    Passed functions to add items to faves

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     update(question, params) {
     this.sendAction('update', question, params);
-  }
+  },
+  addToFaves(item) {
+     this.get('favouriteQuestions').add(item);
+   }
   }
 });

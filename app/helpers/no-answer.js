6233a1116b66dfa20f23f01603071228784907cc
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 16:01:00 +0300

    Fixed no answer display button bug

--- app/helpers/no-answer.js
+++ app/helpers/no-answer.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function qWithA(params) {
   var question = params[0];
 
-  if (question.get('answers').get('length') >= 0) {
+  if (question.get('answers').get('length') === 0) {
     return Ember.String.htmlSafe('<button class="btn btn-danger">0 Answers</button>');
   }
 }

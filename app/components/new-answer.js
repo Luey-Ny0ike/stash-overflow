186e1db9b000530dcc8d2043438b25c3929baae9
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 12:58:33 +0300

    Upvote/Downvote functionality not working FIX THIS LATER!

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   actions:{
     saveAnswer() {
       var params = {
         smartGuy: this.get('smartGuy'),
         date: this.get('date'),
         answer: this.get('answer'),
-        vote:0,
+        vote:20,
         question: this.get('question')
       };
       this.sendAction('saveAnswer', params);
-    }
-  }
+    },
+    upvote(answer) {
+     this.sendAction('upvote', answer);
+   },
+   downvote(answer) {
+     this.sendAction('downvote', answer);
+   }
+ }
 });
--- app/models/answer.js
+++ app/models/answer.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
 smartGuy: DS.attr(),
 datee: DS.attr(),
 answer:DS.attr(),
+vote:DS.attr(),
 question:DS.belongsTo('question', {async: true}),
 });

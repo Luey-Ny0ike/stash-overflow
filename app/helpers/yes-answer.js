1de572ef696c43713c7aa70aba887cc6cf077c9e
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 16:33:24 +0300

    fixed bug to display a button based on the number of answers

--- app/helpers/yes-answer.js
+++ app/helpers/yes-answer.js
@@ -4,7 +4,7 @@ export function yesAnswer(params) {
   var question = params[0];
 
 
-    if (question.get('answers').get('length') >= 0 ){
+    if (question.get('answers').get('length') > 0 ){
     return Ember.String.htmlSafe('<button class="btn btn-success">answers.length></button>');
   }
 }

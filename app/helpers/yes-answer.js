8ce277c109bc46a3ac3c74eced478b9bc15d0501
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 16:30:39 +0300

    Added a button to display number of answers in a question But its not working though

--- app/helpers/yes-answer.js
+++ app/helpers/yes-answer.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
-export function yesAnswer(params/*, hash*/) {
-  return params;
+export function yesAnswer(params) {
+  var question = params[0];
+
+
+    if (question.get('answers').get('length') >= 0 ){
+    return Ember.String.htmlSafe('<button class="btn btn-success">answers.length></button>');
+  }
 }
 
 export default Ember.Helper.helper(yesAnswer);

d0a15f657ef0927f9721eb0173ef26d0cd483729
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 17:15:43 +0300

    included a button to hide add question form

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
     questionFormShow(){
       this.set('addNewQuestion', true);
     },
+    questionFormHide(){
+      this.set('addNewQuestion', false);
+    },
     saveQuestion1(){
       var params ={
         author: this.get('author'),

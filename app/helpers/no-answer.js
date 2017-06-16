400e70b25c29c2c14e09b2ff5f96e26445799678
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 15:57:45 +0300

    Created new helper to tell if question has been answerd before or not put a red button to show unanswerd questions

---
+++ app/helpers/no-answer.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function qWithA(params) {
+  var question = params[0];
+
+  if (question.get('answers').get('length') >= 0) {
+    return Ember.String.htmlSafe('<button class="btn btn-danger">0 Answers</button>');
+  }
+}
+
+export default Ember.Helper.helper(qWithA);

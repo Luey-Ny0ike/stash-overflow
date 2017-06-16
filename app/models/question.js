17c5e3f898ff86f93337a84109731e95e38810d6
lewis nyoike <nyoikelewis@gmail.com>
2017-06-16 08:24:56 +0300

    Passed change on our model to return the title and image as well

--- app/models/question.js
+++ app/models/question.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 author:DS.attr(),
 date:DS.attr(),
+title:DS.attr(),
+image:DS.attr(),
 question:DS.attr(),
 answers:DS.hasMany('answer', { async: true}),
 });

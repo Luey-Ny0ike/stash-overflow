7c51c833b0f88fbf5da103372a79a46e23d94c0a
lewis nyoike <nyoikelewis@gmail.com>
2017-06-18 21:57:55 +0300

    changed to display favourites in the index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    favouriteQuestions: Ember.inject.service(),
   model() {
     return Ember.RSVP.hash({
     questions: this.store.findAll('question'),
@@ -12,7 +13,10 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    }
+    },
+    addToFaves(item) {
+       this.get('favouriteQuestions').add(item);
+     }
 
 
   }
